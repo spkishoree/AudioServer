db5226f340b57f17185b9da0cd0cd51398cdb2d2
spkishoree <kishorecena444@gmail.com>
2021-02-16 09:41:27 +0530

    Update AudioServer.py

--- AudioServer.py
+++ AudioServer.py
@@ -8,10 +8,12 @@ import datetime
 
 app = Flask(__name__)
 api  = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///audio.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///audio.db'  #DB file
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    
 db = SQLAlchemy(app)
 
+
+# Using SQL Alchemy as ORM to create DB tables and other operation on it
 
 class Song(db.Model):
     ID = db.Column(db.Integer,primary_key = True)
